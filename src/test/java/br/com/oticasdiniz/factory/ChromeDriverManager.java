c979e31e4a437feadd21f754c4426b9eb7539f50
Rafael Macabu Viana <rafelgoloa@gmail.com>
2024-06-12 21:39:15 -0300

    fix

--- src/test/java/br/com/oticasdiniz/factory/ChromeDriverManager.java
+++ src/test/java/br/com/oticasdiniz/factory/ChromeDriverManager.java
@@ -12,7 +12,6 @@ public class ChromeDriverManager implements DriverManager {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
         options.addArguments("window-size=1200,1100");
         WebDriver driver = new ChromeDriver(options);
         return driver;
